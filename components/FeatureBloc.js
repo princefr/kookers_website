3a0dae0b59df980dfb4b95e2eb7585e8c6227ef9
ONDONDA Prince <pondonda@gmail.com>
2021-02-07 16:37:18 +0100

    fix stroke typo

--- components/FeatureBloc.js
+++ components/FeatureBloc.js
@@ -39,7 +39,7 @@ function FeaturesBlocks() {
                                 <div className="flex-shrink-0">
                                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-black text-white">
                                     <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                                            <path stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                                             </svg>
                                     </div>
                                 </div>
