cbec7f0c58ad2b38e8dbc5b1cd677ac7e8d66aa1
ONDONDA Prince <pondonda@gmail.com>
2021-02-07 16:41:58 +0100

    changing access color

--- components/HeroHome.js
+++ components/HeroHome.js
@@ -35,8 +35,8 @@ export default function HeroHome() {
                             <div className="sm:text-center lg:text-left">
                                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                                     <span className="block">Des plats authentiques.</span>
-                                    <span className="block text-red-400">Fait maison.</span>
-                                    <span className="block text-red-400 xl:inline">Avec amour.</span>
+                                    <span className="block text-red-700">Fait maison.</span>
+                                    <span className="block text-red-700 xl:inline">Avec amour.</span>
                                 </h1>
 
                                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
