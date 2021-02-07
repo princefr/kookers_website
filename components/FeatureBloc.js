080719db6172b7b9598085078d35840253f48eb6
ONDONDA Prince <pondonda@gmail.com>
2021-02-07 15:07:50 +0100

    change lading page wording

--- components/FeatureBloc.js
+++ components/FeatureBloc.js
@@ -18,17 +18,18 @@ function FeaturesBlocks() {
                             <div className="flex">
                                 <div className="flex-shrink-0">
                                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-black text-white">
-                                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                                         </svg>
+                                    
                                     </div>
                                 </div>
                                 <div className="ml-4">
                                     <dt className="text-lg leading-6 font-medium text-gray-900">
-                                        Choisir un plat selon vos préferences
+                                        Commandez.
             </dt>
                                     <dd className="mt-2 text-base text-gray-500">
-                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                        Commandez des plats maison en fonction de vos préferences alimentaires, commandez à l'avance pour que les chefs puissent acheter des ingrédients frais.
             </dd>
                                 </div>
                             </div>
@@ -36,17 +37,17 @@ function FeaturesBlocks() {
                             <div className="flex">
                                 <div className="flex-shrink-0">
                                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-black text-white">
-                                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                                        </svg>
+                                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                                            </svg>
                                     </div>
                                 </div>
                                 <div className="ml-4">
                                     <dt className="text-lg leading-6 font-medium text-gray-900">
-                                        Acheter selon vos préferences
+                                        Retirez ou faites vous livrer.
             </dt>
                                     <dd className="mt-2 text-base text-gray-500">
-                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                    Retirez votre commande chez le chef kookers à proximité de chez vous ou si elle est proposée, bénéficiez d'une livraison à domicile.
             </dd>
                                 </div>
                             </div>
@@ -55,17 +56,17 @@ function FeaturesBlocks() {
                                 <div className="flex-shrink-0">
                                     <div className="flex items-center justify-center h-12 w-12 rounded-md bg-black text-white">
 
-                                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                                        </svg>
+                                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
                                     </div>
                                 </div>
                                 <div className="ml-4">
                                     <dt className="text-lg leading-6 font-medium text-gray-900">
-                                        Transfers are instant
+                                        Dégustez et répetez
             </dt>
                                     <dd className="mt-2 text-base text-gray-500">
-                                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                    Dégustez votre plat ou votre dessert en toute quiétude, satifisait ? recommencez!
             </dd>
                                 </div>
                             </div>
