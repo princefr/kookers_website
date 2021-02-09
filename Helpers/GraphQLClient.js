03f4e2c58c8b970bb7d450a2cfa5a84f8fc7540a
ONDONDA Prince <pondonda@gmail.com>
2021-02-09 11:30:52 +0100

    change graphql uri

--- Helpers/GraphQLClient.js
+++ Helpers/GraphQLClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-    uri: 'https://46cd409c571a.ngrok.io/graphql',
+    uri: 'https://ed23ed8dd441.ngrok.io/graphql',
     cache: new InMemoryCache()
 });
 
